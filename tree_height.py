ce24a3adac152073934ea12c3c780d151249b8d1
Alens221rdb312 <125511344+Alens221rdb312@users.noreply.github.com>
2023-03-05 12:28:51 +0000

    test1

--- tree_height.py
+++ tree_height.py
@@ -7,22 +7,59 @@ import numpy
 
 def compute_height(n, parents):
     # Write this function
-    max_height = 0
+    children = {}
+    for i in range(n):
+        parent = parents[i]
+        if parent == -1:
+            root = i
+        else:
+            if parent not in children:
+                children[parent] = []
+            children[parent].append(i)
     # Your code here
-    return max_height
-
+    def compute_subtree_height(node):
+        if node not in children:
+            return 1
+        subtree_heights = [compute_subtree_height(child) for child in children[node]]
+        return max(subtree_heights) + 1
+    
+    return compute_subtree_height(root)
 
 def main():
     # implement input form keyboard and from files
+    input_type = input("Input type (k - keyboard, F - file): ").strip().lower()
     
+    
+    if input_type == 'k':
+
+        n = int(input("Enter number of nodes: "))
+        parents = list(map(int, input("Enter parent seperated by space: ").split()))
+    elif input_type == 'f':
+        while True:
+            try:
+                filename = input("Enter file name (without letter 'a'): ")    
+                if 'a' in filename:
+                    raise ValueError("File name cannot contain letter 'a'.")
+    
+                with open(f"folder/{filename}", 'r') as f:
+                    n = int(f.readline().strip())
+                    parents = list(map(int, f.readline().strip().split()))
+                break
+            except FileNotFoundError:
+                print("File not found.")
+            except ValueError as e:
+                print(e)
+        else:
+            print("Invalid input type.")
+            return
+
+        print("Tree height:",compute_height(n, parents))
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
-
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
